3f4729e69dda5f0b1188091696e8fb0b1577e661
Igor <igglike@gmail.com>
2024-05-17 19:20:08 +0600

    Сделал запросы к БД для получения данных и отображения в боте

--- database/queries.py
+++ database/queries.py
@@ -9,3 +9,34 @@ class Queries:
             rating INTEGER
         )
     """
+    DROP_GENRES_TABLE = "DROP TABLE IF EXISTS genres"
+    CREATE_GENRES_TABLE = """
+        CREATE TABLE IF NOT EXISTS genres (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        )
+    """
+    DROP_BOOKS_TABLE = "DROP TABLE IF EXISTS books"
+    CREATE_BOOKS_TABLE = """
+        CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            author TEXT,
+            price FLOAT,
+            picture TEXT,
+            genre_id INTEGER,
+            FOREIGN KEY(genre_id) REFERENCES genres(id)
+        )
+    """
+    POPULATE_GENRES = """
+        INSERT INTO genres (name) VALUES
+            ('Триллер'),
+            ('Хоррор'),
+            ('Фантастика')
+    """
+    POPULATE_BOOKS = """
+        INSERT INTO books (name, author, price, picture, genre_id) VALUES
+        ('Война и мир', 'Л.Н. Толстой', 300.0, 'images/book1.jpg', 1),
+        ('Евгений Онегин', 'А.С. Пушкин', 350.0, 'images/book2.jpg', 2),
+        ('Метро 2033', 'Дмитрий Глуховский', 250.0, 'images/book3.jpg', 3)
+    """
--- handlers/shop.py
+++ handlers/shop.py
@@ -1,5 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command
+from config import database
+from pprint import pprint
 
 
 shop_router = Router()
@@ -14,7 +16,7 @@ async def show_categories(message: types.Message):
                 types.KeyboardButton(text="Триллер")
             ],
             [
-                types.KeyboardButton(text="хоррор"),
+                types.KeyboardButton(text="Хоррор"),
             ]
         ],
         resize_keyboard=True
@@ -28,5 +30,21 @@ book_genres = ("триллер", "хоррор", "фантастика")
 @shop_router.message(F.text.lower().in_(book_genres))
 async def show_triller(message: types.Message):
     genre = message.text
+    print("Пользователь нажал на кнопку", genre)
+    data = await database.fetch(
+        """SELECT * FROM books 
+        INNER JOIN genres ON books.genre_id = genres.id 
+        WHERE genres.name = ?""", 
+        (genre,)
+    )
+    pprint(data)
+    if not data:
+        await message.answer("К сожалению, ничего не нашлось")
+        return
     kb = types.ReplyKeyboardRemove()
-    await message.answer(f"Книги жанра {genre}", reply_markup=kb)
+    await message.answer(f"Книги жанра {genre}:", reply_markup=kb)
+    for book in data:
+        image = types.FSInputFile(book.get("picture"))
+        await message.answer_photo(
+            photo=image, 
+            caption=f"{book['name']} - {book['author']}\nЦена: {book['price']} сом")
